4770c8dfcfa2a9e063c047ef70f4368e5ecddc55
Johnny Shaw <johnny.shaw@live.com>
2023-04-13 21:53:02 -0600

    fix for issue 39

--- VMPlex/VMManager.cs
+++ VMPlex/VMManager.cs
@@ -158,8 +158,12 @@ namespace VMPlex
                     105, // Uptime
                     112 // MemoryAvailable
                 };
+
+                //
+                // N.B. In some circumstances this API can return no error and null output.
+                //
                 uint err = vsms.GetSummaryInformation(infoRequest, CreateSettingsArray(), out IMsvm_SummaryInformation[]? summary);
-                if (err != 0)
+                if (err != 0 || summary == null)
                 {
                     return;
                 }
